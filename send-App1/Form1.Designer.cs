bffb9c97c49ebc1bd51c41e78785344d82dc5fb6
振伟 钟 <tokoyo@live.cn>
2020-10-31 16:45:56 +0800

    添加项目文件。

---
+++ send-App1/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace send_App1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(679, 305);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(641, 381);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 342);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "结果";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(198, 381);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(59, 16);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "未签收";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(289, 381);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 16);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "已签收";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(384, 381);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(59, 16);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "未反馈";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(482, 381);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(59, 16);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "已反馈";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(641, 411);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(96, 16);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "启动定时发送";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ send-App1/Form1.cs
@@ -0,0 +1,200 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace send_App1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton1.Checked = true;
+        }
+
+        private bool checkjson(string indata)
+        {            
+            if (!string.IsNullOrEmpty(indata))
+            {
+                string json = indata;
+
+                if (JsonSplit.IsJson(json))//传入的json串
+                {
+                    MessageBox.Show("json格式合法");
+                    return true;
+                }
+                else
+                {
+                    MessageBox.Show("json格式不合法");
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        //Create the Connection Factory  
+        
+        IConnectionFactory factory = new ConnectionFactory("tcp://localhost:61616/");
+        private void clicksenddata()
+        {
+            try
+            {
+                if (radioButton1.Checked)
+                {
+                    //未签收
+                    string recivestatus = "1";
+
+                    using (IConnection connection = factory.CreateConnection())
+                    {
+                        //Create the Session  
+                        using (ISession session = connection.CreateSession())
+                        {
+                            //Create the Producer for the topic/queue  
+                            IMessageProducer prod = session.CreateProducer(
+                                new Apache.NMS.ActiveMQ.Commands.ActiveMQTopic("vdt_alarm"));
+                            //Send Messages  
+                            ITextMessage msg = prod.CreateTextMessage();
+                            string jsonText = textBox1.Text.ToString();
+                            //if(false == checkjson(jsonText))
+                            //{
+                            //    return;
+                            //}
+                            JObject jObj = JObject.Parse(jsonText);
+                            jObj.Add(new JProperty("rstatus", recivestatus));
+                            msg.Text = JsonConvert.SerializeObject(jObj);
+                            prod.Send(msg, Apache.NMS.MsgDeliveryMode.NonPersistent, Apache.NMS.MsgPriority.Normal, TimeSpan.MinValue);
+
+                            //System.Threading.Thread.Sleep(5000);
+                        }
+                    }
+                }
+                else if (radioButton2.Checked)
+                {
+                    //已签收
+                    string recivestatus = "2";
+
+                    using (IConnection connection = factory.CreateConnection())
+                    {
+                        //Create the Session  
+                        using (ISession session = connection.CreateSession())
+                        {
+                            //Create the Producer for the topic/queue  
+                            IMessageProducer prod = session.CreateProducer(
+                                new Apache.NMS.ActiveMQ.Commands.ActiveMQTopic("vdt_alarm"));
+                            //Send Messages  
+                            ITextMessage msg = prod.CreateTextMessage();
+                            string jsonText = textBox1.Text.ToString();
+                            //if (false == checkjson(jsonText))
+                            //{
+                            //    return;
+                            //}
+                            JObject jObj = JObject.Parse(jsonText);
+                            jObj.Add(new JProperty("rstatus", recivestatus));
+                            msg.Text = JsonConvert.SerializeObject(jObj);
+                            prod.Send(msg, Apache.NMS.MsgDeliveryMode.NonPersistent, Apache.NMS.MsgPriority.Normal, TimeSpan.MinValue);
+
+                            //System.Threading.Thread.Sleep(5000);
+                        }
+                    }
+                }
+                else if (radioButton3.Checked)
+                {
+                    //未反馈
+                    string recivestatus = "3";
+
+                    using (IConnection connection = factory.CreateConnection())
+                    {
+                        //Create the Session  
+                        using (ISession session = connection.CreateSession())
+                        {
+                            //Create the Producer for the topic/queue  
+                            IMessageProducer prod = session.CreateProducer(
+                                new Apache.NMS.ActiveMQ.Commands.ActiveMQTopic("vdt_alarm"));
+                            //Send Messages  
+                            ITextMessage msg = prod.CreateTextMessage();
+                            string jsonText = textBox1.Text.ToString();
+                            //if (false == checkjson(jsonText))
+                            //{
+                            //    return;
+                            //}
+                            JObject jObj = JObject.Parse(jsonText);
+                            jObj.Add(new JProperty("rstatus", recivestatus));
+                            msg.Text = JsonConvert.SerializeObject(jObj);
+                            prod.Send(msg, Apache.NMS.MsgDeliveryMode.NonPersistent, Apache.NMS.MsgPriority.Normal, TimeSpan.MinValue);
+
+                            //System.Threading.Thread.Sleep(5000);
+                        }
+                    }
+                }
+                else if (radioButton4.Checked)
+                {
+                    //已反馈
+                    string recivestatus = "4";
+
+                    using (IConnection connection = factory.CreateConnection())
+                    {
+                        //Create the Session  
+                        using (ISession session = connection.CreateSession())
+                        {
+                            //Create the Producer for the topic/queue  
+                            IMessageProducer prod = session.CreateProducer(
+                                new Apache.NMS.ActiveMQ.Commands.ActiveMQTopic("vdt_alarm"));
+                            //Send Messages  
+                            ITextMessage msg = prod.CreateTextMessage();
+                            string jsonText = textBox1.Text.ToString();
+                            //if (false == checkjson(jsonText))
+                            //{
+                            //    return;
+                            //}
+                            JObject jObj = JObject.Parse(jsonText);
+                            jObj.Add(new JProperty("rstatus", recivestatus));
+                            msg.Text = JsonConvert.SerializeObject(jObj);
+                            prod.Send(msg, Apache.NMS.MsgDeliveryMode.NonPersistent, Apache.NMS.MsgPriority.Normal, TimeSpan.MinValue);
+
+                            //System.Threading.Thread.Sleep(5000);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text = string.Format("{0}", ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//生产者
+            Task.Run(delegate {
+                clicksenddata();
+            });
+            label1.Text = "发送线程启动";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(checkBox1.CheckState == CheckState.Checked)
+            {
+                label1.Text = "循环发送线程启动";
+                Task.Run(delegate {
+                    clicksenddata();
+                });
+                button1.Enabled = false; 
+            }
+            else
+            {
+                label1.Text = "循环发送线程停止";
+                button1.Enabled = true;
+            }
+        }
+    }
+}
